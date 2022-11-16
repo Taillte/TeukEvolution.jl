64ab35b1fcbcb67c4bfe694322f9b59fcc639769
Taillte May <tmay@perimeterinstitute.ca>
2022-11-16 13:53:54 -0500

    prints psi0 at null inf at one angular point

--- sanity_checks.jl
+++ sanity_checks.jl
@@ -2,20 +2,21 @@ using CSV
 using DataFrames
 
 #df = CSV.read("./medpsi0/lin_f_re_2.csv", DataFrame)
-df = CSV.File("./medpsi0/lin_f_im_2.csv") |> Tables.matrix
+df = CSV.File("./hrpsi0/lin_f_re_2.csv") |> Tables.matrix
 
 println(size(df))
 times = df[:,1]
 nx = df[1,2]
 ny = df[1,3]
-nt = 84
+nt = 100
 println("nx = ",nx)
 println("ny = ",ny)
 println("nt = ",size(times))
 
 data = zeros(Float64, trunc(Int,nt))
 for i=1:nt
-	for j=100
+	# j=100
+	for j=30
 		data[trunc(Int, i)] = df[trunc(Int, i),trunc(Int, 3+ (ny-1)*nx +j)]
 	end
 end
