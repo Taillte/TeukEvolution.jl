42f3633ebdebb32786eee7b93e9c47ea33617a2f
rcayuso <rcayuso@perimeterinstitute.ca>
2023-02-15 12:04:36 -0500

    comparing mode content

--- freqeuncy_fits.py
+++ freqeuncy_fits.py
@@ -6,14 +6,24 @@ file1 = np.loadtxt("evol_med/2_Harm_re_2_2.csv",delimiter=",", dtype=float)
 times = file1[:,0]
 Ref_MR = file1[:,1]
 Imf_MR = np.loadtxt("evol_med/2_Harm_im_2_2.csv",delimiter=",", dtype=float)[:,1]
+times_HR = np.loadtxt("evol_high/2_Harm_re_2_2.csv",delimiter=",", dtype=float)[:,0]
 Ref_HR = np.loadtxt("evol_high/2_Harm_re_2_2.csv",delimiter=",", dtype=float)[:,1]
 Imf_HR = np.loadtxt("evol_high/2_Harm_im_2_2.csv",delimiter=",", dtype=float)[:,1]
-Ref_XHR = np.loadtxt("evol_xhigh/2_Harm_re_2_2.csv",delimiter=",", dtype=float)[:,1]
-Imf_XHR = np.loadtxt("evol_xhigh/2_Harm_im_2_2.csv",delimiter=",", dtype=float)[:,1]
-Ref_LR = np.loadtxt("evol_low/2_Harm_re_2_2.csv",delimiter=",", dtype=float)[:,1]
-Imf_LR = np.loadtxt("evol_low/2_Harm_im_2_2.csv",delimiter=",", dtype=float)[:,1]
+#Ref_XHR = np.loadtxt("evol_xhigh/2_Harm_re_2_2.csv",delimiter=",", dtype=float)[:,1]
+#Imf_XHR = np.loadtxt("evol_xhigh/2_Harm_im_2_2.csv",delimiter=",", dtype=float)[:,1]
+#Ref_LR = np.loadtxt("evol_low/2_Harm_re_2_2.csv",delimiter=",", dtype=float)[:,1]
+#Imf_LR = np.loadtxt("evol_low/2_Harm_im_2_2.csv",delimiter=",", dtype=float)[:,1]
 
-print(Ref_LR.shape)
+file1 = np.loadtxt("mode_comparison2/2_Harm_2_2.csv",delimiter=",", dtype=float)
+f_22 = file1[:,1]
+f_23 = np.loadtxt("mode_comparison2/2_Harm_2_3.csv",delimiter=",", dtype=float)[:,1]
+f_24 = np.loadtxt("mode_comparison2/2_Harm_2_4.csv",delimiter=",", dtype=float)[:,1]
+
+
+#Imf_34 = np.loadtxt("mode_comparison/2_Harm_im_3_4.csv",delimiter=",", dtype=float)[:,1]
+
+
+#print(Ref_LR.shape)
 
 def _fit_func(data,Amp,wR,wI,phase):
     total =  Amp * np.sin(wR*times+phase)* np.exp(wI*times) 
@@ -70,21 +80,21 @@ def find_omegaI(dataR,dataI,times):
     print('omegaI = ', slope)
     return Amp,slope
 
-phases_XHR = omega_from_f(Ref_XHR,Imf_XHR,times)
-data_XHR,slope_XHR = find_period(phases_XHR,times)
-amp_XHR,wi_XHR = find_omegaI(Ref_XHR,Imf_XHR,times)
+#phases_XHR = omega_from_f(Ref_XHR,Imf_XHR,times)
+#data_XHR,slope_XHR = find_period(phases_XHR,times)
+#amp_XHR,wi_XHR = find_omegaI(Ref_XHR,Imf_XHR,times)
 
-phases_HR = omega_from_f(Ref_HR,Imf_HR,times)
-data_HR,slope_HR = find_period(phases_HR,times)
-amp_HR,wi_HR = find_omegaI(Ref_HR,Imf_HR,times)
+phases_HR = omega_from_f(Ref_HR,Imf_HR,times_HR)
+data_HR,slope_HR = find_period(phases_HR,times_HR)
+amp_HR,wi_HR = find_omegaI(Ref_HR,Imf_HR,times_HR)
 
 phases_MR = omega_from_f(Ref_MR,Imf_MR,times)
 data_MR,slope_MR = find_period(phases_MR,times)
 amp_MR,wi_MR = find_omegaI(Ref_MR,Imf_MR,times)
 
-phases_LR = omega_from_f(Ref_LR,Imf_LR,times*0.5)
-data_LR,slope_LR = find_period(phases_LR,times*0.5)
-amp_LR,wi_LR = find_omegaI(Ref_LR,Imf_LR,times*0.5)
+#phases_LR = omega_from_f(Ref_LR,Imf_LR,times*0.5)
+#data_LR,slope_LR = find_period(phases_LR,times*0.5)
+#amp_LR,wi_LR = find_omegaI(Ref_LR,Imf_LR,times*0.5)
 
 #phasesHR = omega_from_f(RefHR,ImfHR,timesHR)
 #dataHR,slope2 = find_period(phasesHR,timesHR)
@@ -93,12 +103,13 @@ amp_LR,wi_LR = find_omegaI(Ref_LR,Imf_LR,times*0.5)
 # convergence factor for result
 #wr =  -0.5321879751341276  with err  0.0006461247390380805
 
-print('convergence = ',(slope_MR+ 0.5326002435510183)/(slope_HR+0.5326002435510183))
-print((np.log(np.abs(slope_MR - slope_HR)**-1 *np.abs(slope_XHR-slope_HR)))/np.log(2))
-print((np.log(np.abs(wi_HR - wi_MR)**-1 *np.abs(wi_XHR-wi_HR)))/np.log(2))
+print('convergence = ',np.log((slope_MR+ 0.5326002435510183)/(slope_HR+0.5326002435510183))/np.log(2))
+print('convergence = ',np.log((wi_MR+0.0807929627407481 )/(wi_HR+0.0807929627407481))/np.log(2))
+#print((np.log(np.abs(slope_MR - slope_HR)**-1 *np.abs(slope_XHR-slope_HR)))/np.log(2))
+#print((np.log(np.abs(wi_HR - wi_MR)**-1 *np.abs(wi_XHR-wi_HR)))/np.log(2))
 
-print((np.log(np.abs(slope_MR - slope_LR)**-1 *np.abs(slope_MR-slope_HR)))/np.log(2))
-print((np.log(np.abs(wi_LR - wi_MR)**-1 *np.abs(wi_MR-wi_HR)))/np.log(2))
+#print((np.log(np.abs(slope_MR - slope_LR)**-1 *np.abs(slope_MR-slope_HR)))/np.log(2))
+#print((np.log(np.abs(wi_LR - wi_MR)**-1 *np.abs(wi_MR-wi_HR)))/np.log(2))
 
 from matplotlib.backends.backend_pdf import PdfPages
 
@@ -117,38 +128,68 @@ plt.figure()
 #plt.plot(timesLRn,data2,'o',label='LR')
 #plt.plot(timesLRn,ImfLRn,label='Imf')
 #plt.plot(times,Imf)
-plt.plot(0.5*times,np.log(amp_LR),label='LR')
-plt.plot(times,np.log(amp_HR),label='HR')
-plt.plot(times,np.log(amp_XHR),label='XHR')
+#plt.plot(0.5*times,np.log(amp_LR),label='LR')
+plt.plot(times_HR,np.log(amp_HR),label='HR')
+#plt.plot(times,np.log(amp_XHR),label='XHR')
 plt.plot(times,np.log(amp_MR),label='MR')
 #plt.plot(timesHR,np.log(ampHR),label='HR')
-plt.plot(times,-5-times*0.08,label='slope wi')
+#plt.plot(times,-5-times*0.08,label='slope wi')
 plt.legend()
-plt.ylabel('Phase')
+plt.ylabel('Log Amp')
 plt.xlabel('t')
 pp.savefig()
 pp.close()
 
 pp = PdfPages('./plots/logAmp.pdf')
 plt.figure()
-plt.plot(0.5*times,data_LR,'o',label='LR')
+#plt.plot(0.5*times,data_LR,'o',label='LR')
 plt.plot(times,data_MR,'o',label='MR')
-plt.plot(times,data_HR,'o',label='HR')
-plt.plot(times,data_XHR,'o',label='XHR')
+plt.plot(times_HR,data_HR,'o',label='HR')
+#plt.plot(times,data_XHR,'o',label='XHR')
 plt.legend()
-plt.ylabel('Log Amp(psi0)')
+plt.ylabel('Phase')
 plt.xlabel('t')
 pp.savefig()
 pp.close()
 
 pp = PdfPages('./plots/Repsi0.pdf')
 plt.figure()
-plt.plot(0.5*times,Ref_LR,label='lr')
+#plt.plot(0.5*times,Ref_LR,label='lr')
 plt.plot(times,Ref_MR,label='mr')
-plt.plot(times,Ref_HR,label='hr')
-plt.plot(times,Ref_XHR,label='xhr')
+plt.plot(times_HR,Ref_HR,label='hr')
+#plt.plot(times,Ref_XHR,label='xhr')
 plt.legend()
 plt.ylabel('Re(psi0) - 2Y22')
+plt.xlabel('t')
+pp.savefig()
+pp.close()
+
+
+
+#pp = PdfPages('./plots/Mode_amps.pdf')
+#plt.figure()
+#plt.plot(times,np.log(amp_22),label='(2,2)')
+#plt.plot(times,np.log(amp_23),label='(3,2)')
+#plt.plot(times,np.log(amp_24),label='(4,2)')
+#plt.plot(times,np.log(amp_33),label='(3,3)')
+#plt.plot(times,np.log(amp_34),label='(4,3)')
+#plt.plot(times,-5-times*0.08,label='slope wi')
+#plt.legend()
+#plt.ylabel('Log Amp(l,m)')
+#plt.xlabel('t')
+#pp.savefig()
+#pp.close()
+
+
+pp = PdfPages('./plots/Mode_amps.pdf')
+plt.figure()
+plt.plot(times,f_22,label='(2,2)')
+plt.plot(times,-f_23,label='-(3,2)')
+plt.plot(times,f_24,label='(4,2)')
+#plt.plot(times,Ref_33,label='(3,3)')
+#plt.plot(times,Ref_34,label='(4,3)')
+plt.legend()
+plt.ylabel('A(l,m)')
 plt.xlabel('t')
 pp.savefig()
 pp.close()
--- sanity_checks.jl
+++ sanity_checks.jl
@@ -5,13 +5,13 @@ using CSV
 using DataFrames
 
 #df = CSV.read("./medpsi0/lin_f_re_2.csv", DataFrame)
-df = CSV.File("./evol_high/lin_f_re_2.csv") |> Tables.matrix
+df = CSV.File("./evol_med/lin_f_re_2.csv") |> Tables.matrix
 
 println(size(df))
 times_local = df[:,1]
 ny = trunc(Int, df[1,3])
 nx = trunc(Int, df[1,2])
-nt = trunc(Int,10000)
+nt = trunc(Int,5000)
 spin = 2
 println("nx = ",nx)
 println("ny = ",ny)
@@ -47,9 +47,6 @@ end
 
 
 
-""" Transformation matrix taking R -> Ylm space"""
-yv, wv = FGQ.gausslegendre(trunc(Int, ny))
-nl = num_l(ny)
 """Choosing here single m mode m_ang instead of ?summing """
 m_ang = 2
 lmin = max(abs(spin), abs(m_ang))
@@ -136,34 +133,108 @@ function modes_from_source(filename::String,mv::Integer,lv::Integer)
     df = CSV.File("./$(filename)") |> Tables.matrix
     times = df[:,1]
     data_Ylm = zeros(Float64, trunc(Int,nt))
+    ny_here = trunc(Int, df[1,3])
+    println("index = ", ny_here)
 
+    yv, wv = FGQ.gausslegendre(ny_here)
+    nl = num_l(ny_here)
     for tim = 1:nt
-        f_Ylm = zeros(2,ny)
-        for j = 1:ny
+        f_Ylm = zeros(2,ny_here)
+        for j = 1:ny_here
             for i in [2]
                  f_Ylm[i, j] = 0.0
-                for k = 1:ny
+                for k = 1:ny_here
                     f_Ylm[i, j] +=df[tim, 3+ (i-1)*k+ k ] * swal(spin, mv, lv, yv[k])*wv[k]
                 end
             end
-            data_Ylm[tim] +=f_Ylm[j]
+            data_Ylm[tim] +=f_Ylm[j]/ny_here
         end
         #println(f_Ylm)
         #println(" ")
     end
-    return times,data_Ylm
+    return ny,times,data_Ylm
 end
+
+function compare_modes(filename1::String,filename2::String,mv::Integer)
+    df = CSV.File("./$(filename1)") |> Tables.matrix
+    times = df[:,1]
+    data_Ylm = zeros(Float64, trunc(Int,nt))
+    ny_here = trunc(Int, df[1,3])
+
+    dftwo = CSV.File("./$(filename2)") |> Tables.matrix
+
+    yv, wv = FGQ.gausslegendre(ny_here)
+    #nl = num_l(ny_here)
+    nl = 3
+    lmodes = [2,3,4]
+    mmodes = [2,2,2]
+    println("nl = ",nl)
+    #lmodes = zeros(nl)
+    #data_Ylm = zeros(nl, trunc(Int,nt))
+
+    for lv_index = 1:nl
+	data_Ylm = zeros(Float64,nt)
+        lv_here = lmodes[lv_index]
+	mv_here = mmodes[lv_index]
+	println("mode ",lv_here," ",mv_here)
+        for tim = 1:nt
+            f_Ylm = zeros(2,ny_here)
+            for j = 1:ny_here
+                for i in [2]
+                    f_Ylm[i, j] = 0.0
+                    for k = 1:ny_here
+			f_Ylm[i, j] +=(df[tim, 3+ (i-1)*k+ k ] ^2 + dftwo[tim, 3+ (i-1)*k+ k ]^2) * swal(spin, mv_here, lv_here, yv[k])*wv[k]
+                        #f_Ylm[i, j] +=df[tim, 3+ (i-1)*k+ k ] * swal(spin, mv_here, lv_here, yv[k])*wv[k]
+                    end
+                end
+                data_Ylm[tim] +=f_Ylm[j]/ny_here
+            end
+            #println(f_Ylm)
+            #println(" ")
+        end
+	save_mode( times, nt, ny_here, mv_here, lv_here, "mode_comparison2/2_Harm", data_Ylm)
+    end
+   
+end
+
+function test_mode_projection(lv1::Integer,mv1::Integer,lv2::Integer,mv2::Integer)
+	f_Ylm = zeros(ny)
+	result = 0
+	yv, wv = FGQ.gausslegendre(ny)
+        for j = 1:ny
+                for k = 1:ny
+                        f_Ylm[j] +=swal(spin, mv1, lv1, yv[k])* swal(spin, mv2, lv2, yv[k])*wv[k]
+                end
+                result +=f_Ylm[j]/ny
+        end
+        println(result)
+end
+
 
 #filenames = ["evol_med/lin_f_re_2.csv","evol_med/lin_f_im_2.csv","evol_high/lin_f_re_2.csv","evol_high/lin_f_im_2.csv","evol_xhigh/lin_f_re_2.csv","evol_xhigh/lin_f_im_2.csv"]
 #outputs = ["evol_med/2_Harm_re", "evol_med/2_Harm_im","evol_high/2_Harm_re", "evol_high/2_Harm_im","evol_xhigh/2_Harm_re", "evol_xhigh/2_Harm_im"]
 
-filenames = ["evol_low/lin_f_re_2.csv","evol_low/lin_f_im_2.csv"]
-outputs = ["evol_low/2_Harm_re","evol_low/2_Harm_im"]
+filenames = ["evol_med/lin_f_re_2.csv","evol_med/lin_f_im_2.csv","evol_high/lin_f_re_2.csv","evol_high/lin_f_im_2.csv"]
+outputs = ["evol_med/2_Harm_re","evol_med/2_Harm_im","evol_high/2_Harm_re","evol_high/2_Harm_im"]
 
-for i =1:2
-    times,mode = modes_from_source(filenames[i],2,2)
-    save_mode(times,nt,ny,2,2,outputs[i],mode)
-end
+#for i =1:4
+#    ny_save,times,mode = modes_from_source(filenames[i],2,2)
+#    save_mode(times,nt,ny_save,2,2,outputs[i],mode)
+#end
+
+compare_modes("evol_med/lin_f_re_2.csv","evol_med/lin_f_im_2.csv",2)
+
+test_mode_projection(2,2,2,2)
+
+#test_mode_projection(3,3,3,3)
+#test_mode_projection(3,2,3,2)
+
+test_mode_projection(2,2,3,2)
+test_mode_projection(2,2,4,2)
+
+#test_mode_projection(2,2,3,3)
+#test_mode_projection(3,3,3,2)
+#test_mode_projection(2,2,4,2)
 
 #print(times)
 #println(" ")
